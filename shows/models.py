a8c9146cb44cde277965787ec40db8359c9c6e92
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-01 15:19:03 -0600

    update models

--- shows/models.py
+++ shows/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from django.utils.timezone import now
+
 class Network(models.Model):
     name = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -8,6 +10,6 @@ class Network(models.Model):
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.ForeignKey(Network, related_name="shows", on_delete = models.CASCADE)
-    release_date = models.DateField
+    release_date = models.DateField(default=now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
