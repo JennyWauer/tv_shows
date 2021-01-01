9cd9c5a7a1a9a712ed2d08ec1598f0c10b276940
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-01 14:45:49 -0600

    add network class

--- shows/models.py
+++ shows/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+class Network(models.Model):
+    name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 class Show(models.Model):
     title = models.CharField(max_length=255)
     network = models.ForeignKey(Network, related_name="shows", on_delete = models.CASCADE)
