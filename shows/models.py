bd965940d8e46644d61de7b824854c99ea8d7fc6
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-01 14:44:45 -0600

    add show model

--- shows/models.py
+++ shows/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Show(models.Model):
+    title = models.CharField(max_length=255)
+    network = models.ForeignKey(Network, related_name="shows", on_delete = models.CASCADE)
+    release_date = models.DateField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
