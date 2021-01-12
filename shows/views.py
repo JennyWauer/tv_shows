e9ddd205f14b5d774e9846d3a5a64d743c1a49dd
JennyWauer <64482030+JennyWauer@users.noreply.github.com>
2021-01-12 10:59:19 -0600

    remove validation for first part of the assignment

--- shows/views.py
+++ shows/views.py
@@ -34,30 +34,16 @@ def add_show(request):
     if request.method == 'GET':
         return redirect('/shows')
     if request.method == 'POST':
-        errors = Show.objects.basic_validator(request.POST)
-        if len(errors) > 0:
-            for key, value in errors.items():
-                messages.error(request, value)
-            return redirect('/shows/new')
-        else:
-            Show.objects.create(title=request.POST['title'],network=request.POST['network'],release_date=request.POST['release_date'],desc=request.POST['desc'])
-            return redirect('/shows')
+        Show.objects.create(title=request.POST['title'],network=request.POST['network'],release_date=request.POST['release_date'],desc=request.POST['desc'])
+        return redirect('/shows')
 
 def edit_show(request):
     if request.method == 'GET':
         return redirect('/shows')
     if request.method == 'POST':
-        number = int(request.POST['show_id'])
-        show_to_edit = Show.objects.get(id=request.POST['show_id'])
-        edit_errors = Show.objects.basic_validator(request.POST)
-        if len(edit_errors) > 0:
-            for key, value in edit_errors.items():
-                messages.error(request, value)
-            return render(request, 'edit.html')
-        else:
-            show_to_edit.title = request.POST['title']
-            show_to_edit.network = request.POST['network']
-            show_to_edit.release_date = request.POST['release_date']
-            show_to_edit.desc = request.POST['desc']
-            show_to_edit.save()
-            return redirect('/shows')
+        show_to_edit.title = request.POST['title']
+        show_to_edit.network = request.POST['network']
+        show_to_edit.release_date = request.POST['release_date']
+        show_to_edit.desc = request.POST['desc']
+        show_to_edit.save()
+        return redirect('/shows')
